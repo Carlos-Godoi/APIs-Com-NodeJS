cdef558d4c4bdbea3a0d091571ba578da7c6d511
Carlos-Godoi <168770521+Carlos-Godoi@users.noreply.github.com>
2025-10-10 18:09:57 -0300

    Aula 21 e 22
    Aula 21: Configurar Mongo Atlas.
    
    Aula 22: Reestruturar API.

---
+++ conexao/mongo.js
@@ -0,0 +1,47 @@
+// Importando a class MongoClient do pacote 'mongodb'
+const { MongoClient } = require('mongodb');
+
+// Conexão com o MongoDB, usando o MongoDB Atlas (nuvem).
+const uri = 'mongodb+srv://ccarlosgodoii_db_user:vzamUzE4pkelbpMH@cluster0.s5jte4y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Criando uma instância do MongoClient, que será usada para estabelecer a conexão com o banco de dados.
+const client = new MongoClient(uri);
+
+// Variável para armazenar a coleção 'pessoas' que será usada nas operações CRUD.
+let pessoasCollection;
+
+// Função assíncrona para conectar ao MongoDB.
+async function conectarMongo() {
+    try {
+        // Usando o `await` para aguardar a conexão do MongoDB ser estabelecida através da URI fornecida.
+        await client.connect();
+
+        // Após a conexão bem-sucedida, acessamos o banco de dados 'api_db'.
+        const db = client.db('api_db');
+
+        // Agora que temos o banco de dados, acessamos a coleção 'pessoas' e armazenamos em 'pessoasCollection';
+        pessoasCollection = db.collection('pessoas');
+
+        // Exibimos uma mensagem no console caso a conexão seja bem-sucedida.
+        console.log('MongoDB conectado com sucesso!');
+    } catch (err) {
+        // Se houver algum erro durante a conexão com o MongoDB, ele será capturado aqui.
+        console.error('Erro ao conectar com o MongoDB:', err);
+    }    
+}
+
+// Função para obter a coleção 'pessoas' após a conexão com o MongoDB.
+function getPessoasCollection(){
+    // Se a coleção não foi definida (caso a conexão não tenha ocorrido com sucesso).
+    if (!pessoasCollection) {
+        throw new Error('Coleção não conectada.');
+    }
+
+    // Se a coleção foi conectada, retornamos o objeto da coleção 'pessoas'.
+    return pessoasCollection;
+}
+
+// Exportamos as funções para que possam ser usadas em outros arquivos do projeto.
+// especialmente nas rotas onde precisamos interagir com a coleção 'pessoas' no MongoDB.
+module.exports = { conectarMongo, getPessoasCollection };
+
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Importar módulo de CORS
 const cors = require('cors');
 
+// Importar a função `conectarMongo`do arquivo `conexao/mongo.js`
+const { conectarMongo } = require('./conexao/mongo');
+
 // Cria uma instância di aplicativo Express
 const app = express();
 
@@ -17,7 +20,16 @@ const pessoaRouter = require('./rotas/pessoa');
 app.use(express.json());
 
 // Executa o projeto na porta especificada
-app.listen(8080, () => {
-    console.log('Servidor rodando em http://localhost:8080');
+// app.listen(8080, () => {
+//     console.log('Servidor rodando em http://localhost:8080');
+// });
+ 
+// Chamar a função `conectarMongo()` para estabelecer a conexão.
+conectarMongo().then(() => {
+    
+    // Após a conexão com o MongoDB ser bem-sucedida, o servidor Express é iniciado
+    app.listen(8080, () => console.log('Servidor rondando na porta 8080'));
+}).catch(err => {
+    // Caso ocorra algum erro durante a conexão com o MongoDB ou iniciar o servidor
+console.error('Erro ao conectar ao MongoDB ou iniciar o servidor:', err);
 });
-  
